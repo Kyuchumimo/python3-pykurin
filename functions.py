5b0f6a7b338dd9f25b3ae8b950f45874e118a879
Kyuchumimo <74131798+Kyuchumimo@users.noreply.github.com>
2023-05-10 21:26:17 -0700

    Update functions.py
    Corrections of the following errors:
    *NameError: name 'xrange' is not defined

--- functions.py
+++ functions.py
@@ -3,30 +3,29 @@ import os
 import glob
 import sys
 
-
-
 #Function to load a sprite set and slice it
 def load_and_slice_sprite(w,h,filename):
-	'''
-	load_and_slice_sprite:
-		All the sprites are positioned in a horitzontal file
-		All the frames have the same size (w,h)
-	'''
+    '''
+    load_and_slice_sprite:
+        All the sprites are positioned in a horitzontal file
+        All the frames have the same size (w,h)
+    '''
 
-	images = []
-	master_image = pygame.image.load(os.path.join('sprites', filename))
-	master_w,master_h = master_image.get_size()
+    images = []
+    master_image = pygame.image.load(os.path.join('sprites', filename))
+    master_w,master_h = master_image.get_size()
 
-	for i in xrange(int(master_w/w)):
-		images.append(master_image.subsurface((i*w,0,w,h)).convert_alpha())
+    for i in range(int(master_w/w)):
+        images.append(master_image.subsurface((i*w,0,w,h)).convert_alpha())
 
-	return images
+    return images
 
 def print_mask(mask):
-	w,h = mask.get_size()
-	for y in range(h):
-		for x in range(w):
-			sys.stdout.write(str(mask.get_at((x,y))))
+    w,h = mask.get_size()
+    for y in range(h):
+        for x in range(w):
+            sys.stdout.write(str(mask.get_at((x,y))))
 
-		sys.stdout.write("\n")
-	sys.stdout.write("---------------------------\n")
+        sys.stdout.write("\n")
+    sys.stdout.write("---------------------------\n")
+
