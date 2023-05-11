26fb24ef5efcb0ed8fab4d2ca1d439b8cc824f51
Kyuchumimo <74131798+Kyuchumimo@users.noreply.github.com>
2023-05-10 21:19:09 -0700

    Update cMonster.py
    Corrections of the following errors:
    *TabError: inconsistent use of tabs and spaces in indentation
    *IndentationError: unexpected indent

--- cMonster.py
+++ cMonster.py
@@ -3,67 +3,67 @@ from pygame import mask
 import pygame
 
 class cMonster(pygame.sprite.Sprite):
-	"""
-		Superclass for monsters and items. Controlling basic
-	"""
-	damages_on_touch = True
-	x = 0
-	y = 0
-	rot = 0
-	image = None
-	rect = None
-	baseImage = None
-	mask = None
-	anim = None
-	col_anim = None
-	delete_on_colision = False
+    """
+        Superclass for monsters and items. Controlling basic
+    """
+    damages_on_touch = True
+    x = 0
+    y = 0
+    rot = 0
+    image = None
+    rect = None
+    baseImage = None
+    mask = None
+    anim = None
+    col_anim = None
+    delete_on_colision = False
 
-	def __init__(self,x=0,y=0,rot=0):
-		self.x = x
-		self.y = y
-		self.rot = rot
-
-
-	def onCollision(self,stick,status):
-		"""
-			Default on colision handling.
-			Decreases the player lives
-		"""
-		if not status._DEBUG_DEATH: status.decrease_lives()
+    def __init__(self,x=0,y=0,rot=0):
+        self.x = x
+        self.y = y
+        self.rot = rot
 
 
-	#Function to call on draw update
-	def draw_update(self):
-                if self.col_anim.draw == False:
-			return self.anim.update(time.get_ticks())
-		else:
-			return self.col_anim.update(time.get_ticks())
+    def onCollision(self,stick,status):
+        """
+            Default on colision handling.
+            Decreases the player lives
+        """
+        if not status._DEBUG_DEATH: status.decrease_lives()
 
 
-	def isMonster(self):
-		return True
-
-	def logic_update(self):
-		return False
-
-	def onWallCollision(self):
-		return False
+    #Function to call on draw update
+    def draw_update(self):
+        if self.col_anim.draw == False:
+            return self.anim.update(time.get_ticks())
+        else:
+            return self.col_anim.update(time.get_ticks())
 
 
-	def collides(self,monster):
-		"""
-			Checks if the monster collides with a specific monster
-		"""
-		if self.rect.colliderect(monster.rect):
-			#Here need to pix perfect collision
- 			trectmonst = self.rect.clip(monster.rect).move(-monster.rect.x,-monster.rect.y)
- 			trectstick = self.rect.clip(monster.rect).move(-self.rect.x,-self.rect.y)
+    def isMonster(self):
+        return True
 
-			tmonstmask = mask.from_surface(monster.image.subsurface(trectmonst))
- 			tcurrentmask = mask.from_surface(self.image.subsurface(trectstick))
+    def logic_update(self):
+        return False
 
-			col = tcurrentmask.overlap(tmonstmask,(0,0))
-                	if col == None: return False,0,0
-                	else: return True,col[0]+self.rect.x,col[1]+self.rect.y
+    def onWallCollision(self):
+        return False
 
-		else: return False,0,0
+
+    def collides(self,monster):
+        """
+            Checks if the monster collides with a specific monster
+        """
+        if self.rect.colliderect(monster.rect):
+            #Here need to pix perfect collision
+            trectmonst = self.rect.clip(monster.rect).move(-monster.rect.x,-monster.rect.y)
+            trectstick = self.rect.clip(monster.rect).move(-self.rect.x,-self.rect.y)
+
+            tmonstmask = mask.from_surface(monster.image.subsurface(trectmonst))
+            tcurrentmask = mask.from_surface(self.image.subsurface(trectstick))
+
+            col = tcurrentmask.overlap(tmonstmask,(0,0))
+            if col == None: return False,0,0
+            else: return True,col[0]+self.rect.x,col[1]+self.rect.y
+
+        else: return False,0,0
