c6053a0ac20ca8e89137b3c4a6693b3ce9964f61
Kyuchumimo <74131798+Kyuchumimo@users.noreply.github.com>
2023-05-10 21:49:24 -0700

    Update cItemBouncer.py

--- cItemBouncer.py
+++ cItemBouncer.py
@@ -4,26 +4,26 @@ from cAnimSprite import cAnimSprite
 import functions as BF
 
 class cItemBouncer(cMonster.cMonster):
-	BSPRITEFAC = 1 #this is a little dirty. References the value assigned in cAnimSpriteFactory
-	def __init__(self,x,y,rot):
-		cMonster.cMonster.__init__(self,x,y,rot)
-		self.image      = pygame.image.load("sprites/bouncer.png").convert_alpha()
-		self.rect	= self.image.get_rect().move(x,y)
-		self.mask       = pygame.mask.from_surface(self.image);
-		
-		anim_images     = BF.load_and_slice_sprite(32,32,'bouncer_nanim.png');
-		col_anim_images = BF.load_and_slice_sprite(32,32,'bouncer_anim.png');
+    BSPRITEFAC = 1 #this is a little dirty. References the value assigned in cAnimSpriteFactory
+    def __init__(self,x,y,rot):
+        cMonster.cMonster.__init__(self,x,y,rot)
+        self.image      = pygame.image.load("sprites/bouncer.png").convert_alpha()
+        self.rect       = self.image.get_rect().move(x,y)
+        self.mask       = pygame.mask.from_surface(self.image);
+        
+        anim_images     = BF.load_and_slice_sprite(32,32,'bouncer_nanim.png');
+        col_anim_images = BF.load_and_slice_sprite(32,32,'bouncer_anim.png');
                 
-		self.anim     	= cAnimSprite(anim_images,5)
-		self.col_anim  	= cAnimSprite(col_anim_images,20)
-		self.anim.rect  = self.rect
-		self.col_anim.rect  = self.rect
+        self.anim       = cAnimSprite(anim_images,5)
+        self.col_anim   = cAnimSprite(col_anim_images,20)
+        self.anim.rect  = self.rect
+        self.col_anim.rect  = self.rect
 
-		#Status sets
-		self.col_anim.draw = False
+        #Status sets
+        self.col_anim.draw = False
 
-	def onCollision(self,stick,status):
-		stick.flip_rotation()
+    def onCollision(self,stick,status):
+        stick.flip_rotation()
 
-	def isMonster(self):
-		return False
+    def isMonster(self):
+        return False
